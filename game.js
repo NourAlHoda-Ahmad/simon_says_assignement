65cce82aa20766983a8b724ee3f00d9df9c9f66a
noah305 <nourahmad305@hotmail.com>
2023-11-11 14:49:31 +0200

    Added game functionnalities

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-let colors = ["blue", "yellow", "green", "red"]
+let colors = ["green", "red", "yellow", "blue"]
 let sequenceToFollow = []
 let playerSequence = []
 
@@ -8,64 +8,144 @@ let yellow_btn = document.getElementById("yellow")
 let red_btn = document.getElementById("red")
 
 
-let startGame = false
+// let startGame = true
 let level_title = document.getElementById("level-title")
 let currentLevel = 0
 
 
+//Start level
+function levelUp() {
+    currentLevel += 1
+    playerSequence=[]
+    level_title.innerHTML = `Level ${currentLevel}`
+    //generate random number index between 0 -> 3 
+    let random_index = Math.floor(Math.random() * 4)
+    sequenceToFollow.push(random_index)
+    console.log("level up ", sequenceToFollow)
+
+
+}
 
 
 //Press to Start
 document.addEventListener('keydown', function (event) {
     console.log("keydown entered")
 
-    if (startGame) {
-        console.log(!startGame)
-        level_title.innerHTML = `Level ${currentLevel}`
-        playGame();
+    // if (startGame) {
+    //     console.log(!startGame)
+    //     level_title.innerHTML = `Level ${currentLevel}`
+    //     // playGame();
+    //     levelUp();
 
-        startGame = false
+    //     startGame = false
+    // }
+
+    if (currentLevel <= 0) {
+        // playGame();
+        level_title.innerHTML = `Level ${currentLevel}`
+        levelUp();
     }
 
 });
 
 
 
+
+
 // button_pressed.addEventListener('click',changeButtonStatus)
 //change button color
 function changeButtonStatus(button_pressed) {
-    let button_id=button_pressed.id
-    console.log(button_id)
-    button_pressed.addEventListener('click', function (e) {
+    let button_pressed_name = button_pressed.id
+    let button_pressed_index=colors.indexOf(button_pressed_name)
 
-        //change background to grey
-        button_pressed.classList.add("pressed")
-        setTimeout(function () {
-            button_pressed.classList.remove("pressed")
-        }, 500); // Delay of 3 seconds
+    console.log(button_pressed_name)
 
-        //add audio
-        let sound = new Audio(`../sounds/${button_id}.mp3`);
-        sound.play()
+    console.log(playerSequence, sequenceToFollow)
+    //change background to grey
+    button_pressed.classList.add("pressed")
+    setTimeout(function () {
+        button_pressed.classList.remove("pressed")
+    }, 500); // Delay of 0.5 seconds
 
+    //add audio
+    let sound = new Audio(`../sounds/${button_pressed_name}.mp3`);
+    sound.play()
+
+
+
+    //add chosen button to player sequence
+    playerSequence.push(button_pressed_index)
+    checkSequence()
+
+    // console.log("lup ",sequenceToFollow,random_index)
+    // console.log(playerSequence, sequenceToFollow)
+
+}
+
+function  checkSequence() {
+    console.log(`in check plater seq : ${playerSequence},game seq ${sequenceToFollow}`)
+
+    if(playerSequence.length != sequenceToFollow.length){
+        console.log("game over length")
+        gameOver()
+        // return
+    }
+
+    if(playerSequence.toString()==sequenceToFollow.toString()){
+        console.log("Same sequence")
+        levelUp()
+    }
+
+    else{
+        console.log("wrong sequence")
+        gameOver()
+    }
+
+}
+
+
+//Game Over
+function gameOver(){
+
+
+    let container=document.getElementsByClassName("container")
+    container.classList.add("game-over")
+    setTimeout(function () {
+        button_pressed.classList.remove("pressed")
+    }, 500); // Delay of 0.5 seconds
+
+
+    currentLevel=0
+    // playerSequence=[]
+    sequenceToFollow=[]
+
+   
+
+
+}
+
+
+//Play Game
+function startGame() {
+    console.log("Game", green_btn)
+
+    green_btn.addEventListener('click', function () {
+        changeButtonStatus(green_btn)
+    })
+    blue_btn.addEventListener('click', function () {
+        changeButtonStatus(blue_btn)
+    })
+
+    yellow_btn.addEventListener('click', function () {
+        changeButtonStatus(yellow_btn)
+    })
+
+    red_btn.addEventListener('click', function () {
+        changeButtonStatus(red_btn)
     })
 
 
 }
 
 
-
-//Play Game
-function playGame() {
-    console.log("Game", green_btn)
-
-    green_btn.addEventListener('click', changeButtonStatus(green_btn))
-    blue_btn.addEventListener('click', changeButtonStatus(blue_btn))
-    yellow_btn.addEventListener('click', changeButtonStatus(yellow_btn))
-    red_btn.addEventListener('click', changeButtonStatus(red_btn))
-
-
-}
-
-
-playGame()
+startGame()
