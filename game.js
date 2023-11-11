ff9f5a9467b3aef811498e8e436cd6fda5025577
noah305 <nourahmad305@hotmail.com>
2023-11-11 18:44:14 +0200

    Modified & Added to game.js"

--- game.js
+++ game.js
@@ -6,23 +6,156 @@ let green_btn = document.getElementById("green")
 let blue_btn = document.getElementById("blue")
 let yellow_btn = document.getElementById("yellow")
 let red_btn = document.getElementById("red")
+let all_btn = document.getElementsByClassName("btn")
 
-
-// let startGame = true
+let game_start = true
 let level_title = document.getElementById("level-title")
 let currentLevel = 0
 
 
-//Start level
+//function play audio
+function playSound(button_name) {
+    let sound = new Audio(`../sounds/${button_name}.mp3`);
+    sound.play()
+
+}
+
+//function change background
+function changeButtonColor(button) {
+
+    //change background to grey
+    button.classList.add("pressed")
+    setTimeout(function () {
+        button.classList.remove("pressed")
+    }, 500);
+
+}
+
+
+
+function clickButton() {
+
+    green_btn.addEventListener('click', function () {
+        addPlayerSequence(green_btn)
+    })
+    blue_btn.addEventListener('click', function () {
+        addPlayerSequence(blue_btn)
+    })
+
+    yellow_btn.addEventListener('click', function () {
+        addPlayerSequence(yellow_btn)
+    })
+
+    red_btn.addEventListener('click', function () {
+        addPlayerSequence(red_btn)
+    })
+
+
+}
+
+clickButton()
+
+
+
+
+//Start new level
 function levelUp() {
+
     currentLevel += 1
-    playerSequence=[]
+    playerSequence = []
     level_title.innerHTML = `Level ${currentLevel}`
+
+
     //generate random number index between 0 -> 3 
     let random_index = Math.floor(Math.random() * 4)
-    sequenceToFollow.push(random_index)
-    console.log("level up ", sequenceToFollow)
 
+    sequenceToFollow.push(random_index)
+    console.log("level up ", sequenceToFollow, all_btn[random_index].id)
+    lenght_sequence = sequenceToFollow.length
+
+
+    //change button background
+    const button_generated = all_btn[random_index]
+    changeButtonColor(button_generated)
+
+    //add audio
+    const button_generated_name = all_btn[random_index].id
+    playSound(sequenceToFollow[button_generated_name])
+
+}
+
+
+//change button color
+function addPlayerSequence(button_pressed) {
+
+    let button_pressed_name = button_pressed.id
+    let button_pressed_index = colors.indexOf(button_pressed_name)
+
+    //change background to grey
+    changeButtonColor(button_pressed)
+
+    //add audio
+    playSound(button_pressed_name)
+
+    
+    //add chosen button to player sequence
+    playerSequence.push(button_pressed_index)
+
+    //check
+    checkSequence(playerSequence)
+
+}
+
+
+
+//compare player and game sequences
+function checkSequence() {
+
+    console.log(`in check ,player seq : ${playerSequence},game seq ${sequenceToFollow}`)
+
+    if (playerSequence.length == currentLevel) {
+        console.log(`level = player seq ${playerSequence.length}`)
+
+        if (playerSequence.toString() == sequenceToFollow.toString()) {
+            console.log("Same sequence")
+            levelUp()
+        }
+
+        else {
+            console.log("wrong sequence")
+            gameOver()
+        }
+
+    }
+
+    else {
+
+        if (playerSequence[(playerSequence.length) - 1] != sequenceToFollow[(playerSequence.length) - 1]) {
+            gameOver()
+        }
+    }
+
+}
+
+
+//Game Over
+function gameOver() {
+
+    console.log("GAME OVERRR")
+    document.body.classList.add("game-over")
+    setTimeout(function () {
+        document.body.classList.remove("game-over")
+
+
+    }, 1000);
+
+
+    currentLevel = 0
+    playerSequence = []
+    sequenceToFollow = []
+    game_start = true
+
+    level_title.innerHTML = `Game Over Press Any Key To Restart`
 
 }
 
@@ -31,121 +164,10 @@ function levelUp() {
 document.addEventListener('keydown', function (event) {
     console.log("keydown entered")
 
-    // if (startGame) {
-    //     console.log(!startGame)
-    //     level_title.innerHTML = `Level ${currentLevel}`
-    //     // playGame();
-    //     levelUp();
-
-    //     startGame = false
-    // }
-
-    if (currentLevel <= 0) {
-        // playGame();
+    if (currentLevel <= 0 & game_start == true) {
         level_title.innerHTML = `Level ${currentLevel}`
         levelUp();
+        game_start = false
     }
 
 });
-
-
-
-
-
-// button_pressed.addEventListener('click',changeButtonStatus)
-//change button color
-function changeButtonStatus(button_pressed) {
-    let button_pressed_name = button_pressed.id
-    let button_pressed_index=colors.indexOf(button_pressed_name)
-
-    console.log(button_pressed_name)
-
-    console.log(playerSequence, sequenceToFollow)
-    //change background to grey
-    button_pressed.classList.add("pressed")
-    setTimeout(function () {
-        button_pressed.classList.remove("pressed")
-    }, 500); // Delay of 0.5 seconds
-
-    //add audio
-    let sound = new Audio(`../sounds/${button_pressed_name}.mp3`);
-    sound.play()
-
-
-
-    //add chosen button to player sequence
-    playerSequence.push(button_pressed_index)
-    checkSequence()
-
-    // console.log("lup ",sequenceToFollow,random_index)
-    // console.log(playerSequence, sequenceToFollow)
-
-}
-
-function  checkSequence() {
-    console.log(`in check plater seq : ${playerSequence},game seq ${sequenceToFollow}`)
-
-    if(playerSequence.length != sequenceToFollow.length){
-        console.log("game over length")
-        gameOver()
-        // return
-    }
-
-    if(playerSequence.toString()==sequenceToFollow.toString()){
-        console.log("Same sequence")
-        levelUp()
-    }
-
-    else{
-        console.log("wrong sequence")
-        gameOver()
-    }
-
-}
-
-
-//Game Over
-function gameOver(){
-
-
-    let container=document.getElementsByClassName("container")
-    container.classList.add("game-over")
-    setTimeout(function () {
-        button_pressed.classList.remove("pressed")
-    }, 500); // Delay of 0.5 seconds
-
-
-    currentLevel=0
-    // playerSequence=[]
-    sequenceToFollow=[]
-
-   
-
-
-}
-
-
-//Play Game
-function startGame() {
-    console.log("Game", green_btn)
-
-    green_btn.addEventListener('click', function () {
-        changeButtonStatus(green_btn)
-    })
-    blue_btn.addEventListener('click', function () {
-        changeButtonStatus(blue_btn)
-    })
-
-    yellow_btn.addEventListener('click', function () {
-        changeButtonStatus(yellow_btn)
-    })
-
-    red_btn.addEventListener('click', function () {
-        changeButtonStatus(red_btn)
-    })
-
-
-}
-
-
-startGame()
